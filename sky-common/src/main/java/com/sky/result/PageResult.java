942482dc1aa78debe36762b6f7ae2c2ef30a27e4
wanwu <1842878721@qq.com>
2026-04-02 03:18:23 +0800

    员工分页查询业务代码开发（并添加部分注释）

--- sky-common/src/main/java/com/sky/result/PageResult.java
+++ sky-common/src/main/java/com/sky/result/PageResult.java
@@ -13,6 +13,8 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//都是 Lombok 提供的注解 编译时自动生成一些常见的样板代码， ，@Data Lombok 会自动为这个类生成这些常用方法：所有字段的 getter、setter、toString()、equals()、hashCode()
+//@AllArgsConstructor 的意思是“生成全参构造方法”  @NoArgsConstructor “生成无参构造方法”。是先创建空对象，后面再一个个赋值
 public class PageResult implements Serializable {
 
     private long total; //总记录数
--- sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.sky.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -32,10 +34,11 @@ public class Employee implements Serializable {
 
     private Integer status;
 
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //Jackson 提供的注解，指定某个字段在进行 JSON 序列化或反序列化时采用什么时间格式。
     private LocalDateTime createTime;
 
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 
     private Long createUser;
